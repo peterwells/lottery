bfffb1ca064b076b609e04e06de551f6f7823fe5
Peter Wells <pdwells08@gmail.com>
2017-06-09 15:52:18 -0400

    creating result objects from values in file

--- app/controllers/update_results_controller.rb
+++ app/controllers/update_results_controller.rb
@@ -5,11 +5,20 @@ class UpdateResultsController < ApplicationController
     #create new record if needed
     #close file
     @result_string = "hello world";
-    result_arr = [];
-    File.open(Rails.root.join('results').to_s + '/numbers.csv'){|file|  result_arr = file.readlines;}
+    resultStrArr = [];
+    File.open(Rails.root.join('results').to_s + '/numbers.csv'){|file|  resultStrArr = file.readlines;}
 
-    result_arr.each  do |s|
-      @result_string += s;
+    @resultArr = [];
+    resultStrArr.each  do |s|
+      result_row = s.split(',');
+      newResult = WeeklyResult.new();
+      newResult.drawing_date = Date.strptime(result_row[0], '%m/%d/%Y');
+      newResult.number1 = result_row[1];
+      newResult.number2 = result_row[2];
+      newResult.number3 = result_row[3];
+      newResult.number4 = result_row[4];
+      newResult.number5 = result_row[5];
+      @resultArr.push(newResult);
     end
 
   end
