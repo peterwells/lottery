50ec2a73cda59ab0b5ff8a757e37a3552c04625a
Peter Wells <pdwells08@gmail.com>
2017-06-09 12:12:27 -0400

    added a place for the results file to live. output dummy value to index.

--- app/controllers/update_results_controller.rb
+++ app/controllers/update_results_controller.rb
@@ -1,4 +1,9 @@
 class UpdateResultsController < ApplicationController
   def index
+    #open results file
+    #iterate through
+    #create new record if needed
+    #close file
+    @result_string = "hello world";
   end
 end
