95b5694543bad68f810455dcd1e9af624e4ed07a
Peter Wells <pdwells08@gmail.com>
2017-06-09 13:34:50 -0400

    reading in results file

--- app/controllers/update_results_controller.rb
+++ app/controllers/update_results_controller.rb
@@ -5,5 +5,12 @@ class UpdateResultsController < ApplicationController
     #create new record if needed
     #close file
     @result_string = "hello world";
+    result_arr = [];
+    File.open(Rails.root.join('results').to_s + '/numbers.csv'){|file|  result_arr = file.readlines;}
+
+    result_arr.each  do |s|
+      @result_string += s;
+    end
+
   end
 end
