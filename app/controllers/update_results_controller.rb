cf2abb2c699a3c7c5e85b8da1134a17f8e6a03e0
Peter Wells <pdwells08@gmail.com>
2017-06-16 16:30:36 -0400

    only inserting if record for that date does not exist

--- app/controllers/update_results_controller.rb
+++ app/controllers/update_results_controller.rb
@@ -7,7 +7,7 @@ class UpdateResultsController < ApplicationController
     @result_string = "hello world";
     resultStrArr = [];
     File.open(Rails.root.join('results').to_s + '/numbers.csv'){|file|  resultStrArr = file.readlines;}
-
+    @ctr = 0;
     @resultArr = [];
     resultStrArr.each  do |s|
       result_row = s.split(',');
@@ -18,7 +18,14 @@ class UpdateResultsController < ApplicationController
       newResult.number3 = result_row[3];
       newResult.number4 = result_row[4];
       newResult.number5 = result_row[5];
+      newResult.powerball = result_row[6].split(':')[1];
       @resultArr.push(newResult);
+      existing = WeeklyResult.where(drawing_date: newResult.drawing_date).first
+
+      if existing.nil? then
+        newResult.save!
+        @ctr = @ctr + 1;
+      end
     end
 
   end
--- app/models/weekly_result.rb
+++ app/models/weekly_result.rb
@@ -1,2 +1,5 @@
 class WeeklyResult < ApplicationRecord
+  def to_s
+    return self.number1.to_s + ' ' + self.number2.to_s + ' ' + self.number3.to_s + ' ' + self.number4.to_s + ' ' + self.number5.to_s + ' ' + self.powerball.to_s;
+  end
 end
